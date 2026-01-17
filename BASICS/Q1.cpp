3a3cbc7aef147cbb029b017a51cf5a7e62878341
Gaurav Kumar <neonxgaurav9@gmail.com>
2026-01-18 00:36:06 +0530

    Initial commit - C++ basics

---
+++ BASICS/Q1.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+using namespace std;
+
+int main(){
+    // int x;
+    // cout << "Enter The Number : ";
+    // cin >> x;
+    // cout << x << endl;
+    // return 0;
+
+
+    // int x;
+    // cin >> x;
+
+    // if(x > 0){
+    //     cout << "High";
+    // } else{
+    //     cout << "Low";
+    // }
+
+    // for(int i = 0; i<= 5; i++){
+    //     cout <<i;
+    // }
+
+    // int arr [5] = {1,2,3,4,5};
+    // for(int i = 0; i<5; i++){
+    //     cout << arr[i];
+    // }
+
+    vector<int> v;
+
+    v.
+
+
+}
