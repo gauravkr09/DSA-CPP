0b97c77824c993de306d5e333aa9358e6a360c01
Gaurav Kumar <neonxgaurav9@gmail.com>
2026-01-31 01:48:07 +0530

    Basic

---
+++ 1-Arrays/Q1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int maxElement = arr[0];
+
+    for(int i = 1; i<n; i++){
+       if(maxElement < arr[i]){
+          maxElement = arr[i];
+       }
+    }
+    cout << maxElement;
+    return 0;
+    
+}
--- BASICS/Q1.cpp
+++ BASICS/Q1.cpp
@@ -3,21 +3,43 @@
 using namespace std;
 
 int main(){
-    int n;
-    cin >> n;
-    int arr[n];
+    // int x;
+    // cout << "Enter The Number : ";
+    // cin >> x;
+    // cout << x << endl;
+    // return 0;
 
-    for(int i = 0; i<n; i++){
-        cin >> arr[i];
-    }
 
-    int maxElement = arr[0];
+    // int x;
+    // cin >> x;
 
-    for(int i = 1; i<n; i++){
-       if(maxElement < arr[i]){
-          maxElement = arr[i];
-       }
-    }
-    cout << maxElement;
-    return 0;
+    // if(x > 0){
+    //     cout << "High";
+    // } else{
+    //     cout << "Low";
+    // }
+
+    // for(int i = 0; i<= 5; i++){
+    //     cout <<i;
+    // }
+
+    // int arr [5] = {1,2,3,4,5};
+    // for(int i = 0; i<5; i++){
+    //     cout << arr[i];
+    // }
+
+    // vector<int> v;
+
+    // v.push_back(10);
+    // v.push_back(20);
+    // v.push_back(30);
+
+    // cout << v.size() << endl;
+
+    // return 0;
+
+    for(int i = 0; i < v.size(); i++) {
+    cout << v[i] << " ";
+}
+
 }
