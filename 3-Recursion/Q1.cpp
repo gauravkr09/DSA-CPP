5b9c14b34d34a931b55cde42c7fe4920c49cbcdc
Gaurav Kumar <neonxgaurav9@gmail.com>
2026-07-29 23:46:31 +0530

    Sum of First N number

--- 3-Recursion/Q1.cpp
+++ 3-Recursion/Q1.cpp
@@ -1,15 +1,34 @@
+// print linearly from 1 to N
+
+// #include <iostream>
+// using namespace std;
+
+// void print(int i, int N){
+//     if(i>N)
+//     return;
+//     cout<<i<<" ";
+//     print(i+1,N);
+
+// }
+// int main(){
+//     int N = 5;
+//     print(1,N);
+//     return 0;
+// }   
+
+// print from N to 1
 #include <iostream>
 using namespace std;
 
-void print(int i, int N){
-    if(i>N)
-    return;
-    cout<<i<<"";
-    print(i+1,N);
+void print(int N){
+    if (N==0)
+    return ;
+    cout<<N<<" ";
+    print(N-1);
 
 }
 int main(){
     int N = 5;
-    print(1,N);
+    print(N);
     return 0;
-}   
+}
